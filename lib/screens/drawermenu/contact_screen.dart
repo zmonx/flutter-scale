ab458c84477b3311f98bf0a6722745900483c6a2
zmonx <chumpitak@gmail.com>
2022-10-19 13:14:16 +0700

    google maps

--- lib/screens/drawermenu/contact_screen.dart
+++ lib/screens/drawermenu/contact_screen.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ContactScreen extends StatefulWidget {
   const ContactScreen({super.key});
@@ -10,14 +13,26 @@ class ContactScreen extends StatefulWidget {
 }
 
 class _ContactScreenState extends State<ContactScreen> {
+// create maps conroller
+
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
-       return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Contact'),
       ),
-      body: Center(
-        child: Text("Contact Screen"),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(13.783548, 100.54655),
+          zoom: 20,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        mapType: MapType.hybrid,
+
       ),
     );
   }
